b9f56a5b8f0fa30852a23ae12a988e03d26115f8
Breta01 <breta.hajek@gmail.com>
2022-11-13 18:14:36 +0800

    Add multi round training

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MNIST Demo
 Demo testing FELT on MNIST dataset
+
+
+## Run
+First install **Python 3.9** and requirements as:
+```bash
+pip install -r requirements
+```
+
+Run the training script
+```bash
+python src/train.py
+```
--- src/train.py
+++ src/train.py
@@ -12,7 +12,8 @@ from feltlabs.model import load_model
 from nacl.public import PrivateKey
 from sklearn.metrics import accuracy_score
 
-N_PARTITIONS = 10
+N_PARTITIONS = 3
+ITERATIONS = 2
 
 aggregation_key = PrivateKey.generate()
 
@@ -21,6 +22,27 @@ model_def = {
     "model_name": "LogisticRegression",
     "init_params": {"max_iter": 100},
 }
+
+# model_def = {
+#     "model_type": "sklearn",
+#     "model_name": "MLPClassifier",
+#     "init_params": {"hidden_layer_sizes": [50, 50], "max_iter": 50, "warm_start": True},
+# }
+
+
+(x_train, y_train), (x_test, y_test) = get_mnist_data()
+
+
+def model_test(model, name=""):
+    y_pred = model.predict(x_test)
+    y_pred = np.rint(y_pred)
+
+    print(f"\nModel - {name}")
+    print(y_test)
+    print(y_pred)
+    acc = accuracy_score(y_test, y_pred)
+    print("Accuracy", acc)
+    return acc
 
 
 def test_training(n_partitions: int = 3):
@@ -41,61 +63,53 @@ def test_training(n_partitions: int = 3):
     with open(input_folder.parent / "algoCustomData.json", "w") as f:
         json.dump(model_def, f)
 
-    ### Training section ###
-    args_str = f"--output_folder {output_folder}"
-    args_str += f" --input_folder {input_folder.parent}"
-    args_str += f" --data_type pickle"
-    args_str += f" --aggregation_key {bytes(aggregation_key.public_key).hex()}"
+    for i in range(ITERATIONS):
+        ### Training section ###
+        args_str = f"--output_folder {output_folder}"
+        args_str += f" --input_folder {input_folder.parent}"
+        args_str += f" --data_type pickle"
+        args_str += f" --aggregation_key {bytes(aggregation_key.public_key).hex()}"
 
-    seeds = []
-    local_models = []  # Only for testing
-    files = get_mnist_datafiles(n_partitions)
+        seeds = []
+        local_models = []  # Only for testing
+        files = get_mnist_datafiles(n_partitions)
 
-    for i, file_path in enumerate(files):
-        # Move file to input folder as train train dataset
-        file_path.rename(input_folder / "0")
+        for i, file_path in enumerate(files):
+            # Move file to input folder as train train dataset
+            file_path.rename(input_folder / "0")
 
-        seeds.append(random.randint(0, 1000000))
+            seeds.append(random.randint(0, 1000000))
 
-        args_str_final = f"{args_str} --seed {seeds[-1]}"
-        enc_model = train.main(args_str_final.split(), f"{i}")
-        local_models.append(enc_model)
+            args_str_final = f"{args_str} --seed {seeds[-1]}"
+            enc_model = train.main(args_str_final.split(), f"{i}")
+            local_models.append(enc_model)
 
-    ### Aggregation section ###
-    args_str = f"--output_folder {output_final}"
-    args_str += f" --input_folder {output_folder.parent}"
-    args_str += f" --private_key {bytes(aggregation_key).hex()}"
+        ### Aggregation section ###
+        args_str = f"--output_folder {output_final}"
+        args_str += f" --input_folder {output_folder.parent}"
+        args_str += f" --private_key {bytes(aggregation_key).hex()}"
 
-    aggregate.main(args_str.split(), "model")
+        aggregate.main(args_str.split(), "model")
 
-    ### Test final results ###
-    final_model = load_model(output_final / "model")
-    final_model.remove_noise_models(seeds)
+        ### Test final results ###
+        final_model = load_model(output_final / "model")
+        final_model.remove_noise_models(seeds)
 
-    (x_train, y_train), (x_test, y_test) = get_mnist_data()
+        final_model.export_model(input_folder.parent / "algoCustomData.json")
 
-    def model_test(model, name=""):
-        y_pred = model.predict(x_test)
-        y_pred = np.rint(y_pred)
+        model_test(final_model, "aggregated")
 
-        print(f"\nModel - {name}")
-        acc = accuracy_score(y_test, y_pred)
-        print("Accuracy", acc)
-        return acc
-
-    model_test(final_model, "aggregated")
+        # Test local models
+        for i, (enc_model, seed) in enumerate(zip(local_models, seeds)):
+            data = decrypt_nacl(bytes(aggregation_key), enc_model)
+            model = load_model(data)
+            model.remove_noise_models([seed])
+            model_test(model, f"local_model_{i}")
 
     ### Fully trained model ###
     new_model = load_model(input_folder.parent / "algoCustomData.json")
     new_model.fit(x_train, y_train)
     model_test(new_model, "full_data")
-
-    # Test local models
-    for i, (enc_model, seed) in enumerate(zip(local_models, seeds)):
-        data = decrypt_nacl(bytes(aggregation_key), enc_model)
-        model = load_model(data)
-        model.remove_noise_models([seed])
-        model_test(model, f"local_model_{i}")
 
 
 if __name__ == "__main__":
